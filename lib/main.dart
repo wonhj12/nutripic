473bf216052c8d629892b0b787ef043e3b9f700c
wonhj12 <wonhj12@naver.com>
2024-09-24 00:38:17 +0900

    feat: firebase 사용이 가능하도록 main에 initializeApp 추가 - main에서 dotenv 로딩

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nutripic/utils/app_router.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp();
+
   runApp(const MainApp());
 }
 
