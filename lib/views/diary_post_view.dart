d66e5cd468e258d03d32d5f6e0d8b5264db2668e
chsein <chltpdls4087@gmail.com>
2024-10-04 14:49:27 +0900

    feat: 날짜, 게시글 추가

--- lib/views/diary_post_view.dart
+++ lib/views/diary_post_view.dart
@@ -19,22 +19,47 @@ class _DiaryPostViewState extends State<DiaryPostView> {
     DiaryPostViewModel diaryPostViewModel = context.watch<DiaryPostViewModel>();
 
     return Scaffold(
-      body: Column(
-        children: [
-          const CustomAppBar(title: '새로운 게시물'),
-          GestureDetector(
-            onTap: () {
-              diaryPostViewModel.showCameraSelectModal(context);
-            },
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20.0),
-              child: Image.file(
-                File(widget.imagePath),
-                height: 300,
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CustomAppBar(title: '새로운 게시물'),
+            GestureDetector(
+              onTap: () {
+                diaryPostViewModel.showCameraSelectModal(context);
+              },
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: Image.file(
+                  File(widget.imagePath),
+                  height: 300,
+                ),
               ),
             ),
-          ),
-        ],
+            const SizedBox(
+              height: 15.0,
+            ),
+            Text("10월 4일"),
+            const SizedBox(
+              height: 15.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: TextFormField(
+                maxLines: 5,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.all(20.0),
+                  hintText: '게시글을 작성하세요',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                //키보드 설정
+                textInputAction: TextInputAction.done,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
