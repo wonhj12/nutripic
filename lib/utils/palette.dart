095c9c0ec24a4750b0c5c1a4888b6229487ce632
wonhj12 <wonhj12@naver.com>
2024-10-16 19:37:32 +0900

    feat: 색상, 폰트 팔레트 추가

---
+++ lib/utils/palette.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  // Component colors
+  final Color primay = const Color(0xFFA8E6CF);
+  final Color secondary = const Color(0xFFFB934E);
+  final Color sub = const Color(0xFF296C53);
+  final Color delete = const Color(0xFFF85B49);
+  final Color background = const Color(0xFFF1FAF9);
+
+  // Label colors
+  final Color black = const Color(0xFF252525);
+  final Color gray900 = const Color(0xFF2E3137);
+  final Color gray800 = const Color(0xFF444554);
+  final Color gray700 = const Color(0xFF5B5D6B);
+  final Color gray500 = const Color(0xFF777986);
+  final Color gray400 = const Color(0xFF9496A1);
+  final Color gray200 = const Color(0xFFB3B5BD);
+  final Color gray100 = const Color(0xFFDBDDE7);
+  final Color gray50 = const Color(0xFFF9F9F9);
+  final Color white = const Color(0xFFFFFFFF);
+
+  // Text Styles
+  final TextStyle heading =
+      const TextStyle(fontSize: 24, fontWeight: FontWeight.w700);
+  final TextStyle title =
+      const TextStyle(fontSize: 20, fontWeight: FontWeight.w700);
+  final TextStyle subtitle =
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.w600);
+  final TextStyle body =
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.w400);
+}
