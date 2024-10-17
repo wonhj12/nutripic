adc154c572d410eca3b0e71646b6bdc5263805b4
wonhj12 <wonhj12@naver.com>
2024-10-17 14:07:33 +0900

    mod: shadow 추가 - 주석 설명 추가

--- lib/utils/palette.dart
+++ lib/utils/palette.dart
@@ -1,23 +1,51 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  // Component colors
+  /* Component colors */
+  /// #A8E6CF
   static const Color primary = Color(0xFFA8E6CF);
+
+  /// #FB934E
   static const Color secondary = Color(0xFFFB934E);
+
+  /// #296C53
   static const Color sub = Color(0xFF296C53);
+
+  /// #F85B49
   static const Color delete = Color(0xFFF85B49);
+
+  /// #F1FAF9
   static const Color background = Color(0xFFF1FAF9);
 
-  // Label colors
+  /* Label colors */
+  /// #252525
   static const Color black = Color(0xFF252525);
+
+  /// #2E3137
   static const Color gray900 = Color(0xFF2E3137);
+
+  /// #444554
   static const Color gray800 = Color(0xFF444554);
+
+  /// #5B5D6B
   static const Color gray700 = Color(0xFF5B5D6B);
+
+  /// #777986
   static const Color gray500 = Color(0xFF777986);
+
+  /// #9496A1
   static const Color gray400 = Color(0xFF9496A1);
+
+  /// #B3B5BD
   static const Color gray200 = Color(0xFFB3B5BD);
+
+  /// #DBDDE7
   static const Color gray100 = Color(0xFFDBDDE7);
+
+  /// #F9F9F9
   static const Color gray50 = Color(0xFFF9F9F9);
+
+  /// #FFFFFF
   static const Color white = Color(0xFFFFFFFF);
 
   /* Text Styles */
@@ -41,4 +69,8 @@ class Palette {
   /// <br />Font weight: Regular
   static const TextStyle body =
       TextStyle(fontSize: 14, fontWeight: FontWeight.w400);
+
+  /* Shadows */
+  static BoxShadow shadow =
+      BoxShadow(color: gray200.withOpacity(0.25), blurRadius: 15);
 }
