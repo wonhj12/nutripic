810ba1eadff405e1042ace755c83880abd251257
wonhj12 <wonhj12@naver.com>
2024-10-24 16:13:01 +0900

    feat: Food 오브젝트 생성

--- lib/objects/food.dart
+++ lib/objects/food.dart
@@ -1,1 +1,33 @@
-class Food {}
+class Food {
+  /// 식재료 이름
+  final String name;
+
+  /// 식재료 수
+  final int count;
+
+  /// 식재료 사진 경로
+  final String imageName;
+
+  /// 유통기한
+  final DateTime expireDate;
+
+  Food({
+    required this.name,
+    required this.count,
+    required this.imageName,
+    required this.expireDate,
+  });
+
+  /// 유통기한 마감 d-day를 반환하는 함수
+  int dDay() {
+    return expireDate.difference(DateTime.now()).inDays;
+  }
+
+  @override
+  String toString() {
+    return 'name: $name, '
+        'count: $count, '
+        'imageName: $imageName, '
+        'expireDate: $expireDate';
+  }
+}
