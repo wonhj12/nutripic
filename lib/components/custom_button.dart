eec0e1bacc3d7219f4930086f328e9d9ac854828
wonhj12 <wonhj12@naver.com>
2024-10-24 16:08:43 +0900

    mod: CustomButton 타입 enum 추가 - normal (default): 일반적으로 사용 (sub 초록색) - cancel: 취소 (회색)
    - delete: 삭제 (빨강)

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:nutripic/utils/palette.dart';
 
+/// Custom Button 타입
+enum ButtonType {
+  normal(Palette.sub),
+  cancel(Palette.gray400),
+  delete(Palette.delete);
+
+  final Color color;
+  const ButtonType(this.color);
+}
+
 class CustomButton extends StatelessWidget {
   final String label;
   final double? width;
   final double? height;
+  final ButtonType type;
   final Function()? onPressed;
   const CustomButton({
     super.key,
     required this.label,
     this.width = 55,
     this.height = 30,
+    this.type = ButtonType.normal,
     required this.onPressed,
   });
 
@@ -22,7 +34,7 @@ class CustomButton extends StatelessWidget {
       child: FilledButton(
         onPressed: onPressed,
         style: FilledButton.styleFrom(
-          backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+          backgroundColor: type.color,
           padding: EdgeInsets.zero,
         ),
         child: Text(label, style: Palette.body),
