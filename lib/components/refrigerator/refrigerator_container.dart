2b9adf99722ac36e49601b69123f7b4acd35f12f
wonhj12 <wonhj12@naver.com>
2024-10-24 16:11:46 +0900

    mod: RefrigeratorContainer에서 식재료 선택을 위한 함수 파라미터 추가

--- lib/components/refrigerator/refrigerator_container.dart
+++ lib/components/refrigerator/refrigerator_container.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:nutripic/components/food_tile.dart';
+import 'package:nutripic/components/refrigerator/food_tile.dart';
 import 'package:nutripic/objects/food.dart';
 import 'package:nutripic/utils/palette.dart';
 
 class RefrigeratorContainer extends StatelessWidget {
+  /// 전체 식재료 리스트
   final List<Food> foods;
+
+  /// 선택된 식재료 set (중복 방지)
+  final Set<Food> selectedFoods;
+
+  /// 새로운 식재로 추가
   final Function()? addFood;
+
+  /// 식재료 선택시 콜백 함수
+  final Function(Food) selectFood;
   const RefrigeratorContainer({
     super.key,
     required this.foods,
+    required this.selectedFoods,
     required this.addFood,
+    required this.selectFood,
   });
 
   @override
@@ -43,12 +54,16 @@ class RefrigeratorContainer extends StatelessWidget {
             // 식재료 리스트가 있으면 GridView 생성
             : GridView.builder(
                 itemCount: foods.length,
+                physics: const NeverScrollableScrollPhysics(),
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   mainAxisSpacing: 25,
                   crossAxisCount: 3,
                 ),
-                itemBuilder: (context, index) =>
-                    const FoodTile(src: null, name: '식재료명'),
+                itemBuilder: (context, index) => FoodTile(
+                  food: foods[index],
+                  isSelected: selectedFoods.contains(foods[index]),
+                  select: selectFood,
+                ),
               ),
       ),
     );
