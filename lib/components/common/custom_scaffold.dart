6871eee909e4c6c9edc833fd5f72bcd744426685
wonhj12 <wonhj12@naver.com>
2024-10-28 15:52:51 +0900

    mod: CustomTextField를 조금 더 범용적으로 사용 가능하도록 수정 - TextFieldType enum 생성

--- lib/components/common/custom_scaffold.dart
+++ lib/components/common/custom_scaffold.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class CustomScaffold extends StatelessWidget {
   final PreferredSizeWidget? appBar;
+  final bool? resizeToAvoidBottomInset;
   final Widget? body;
 
   /// ### Padding, margin 등 공통 설정 값이 적용된 Scaffold
@@ -9,16 +10,23 @@ class CustomScaffold extends StatelessWidget {
   const CustomScaffold({
     super.key,
     this.appBar,
+    this.resizeToAvoidBottomInset,
     this.body,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: body,
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: appBar,
+        resizeToAvoidBottomInset: resizeToAvoidBottomInset,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: body,
+          ),
+        ),
       ),
     );
   }
