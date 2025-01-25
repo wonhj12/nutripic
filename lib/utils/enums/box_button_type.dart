a84ce2d28656e12ca4ba598055268fba28cd318c
wonhj12 <wonhj12@naver.com>
2025-01-25 14:04:03 +0900

    design: select button -> box button 이름 변경 및 크기 옵션 추가

---
+++ lib/utils/enums/box_button_type.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:nutripic/utils/palette.dart';
+
+/// Select Button 타입
+enum BoxButtonType {
+  normal(Palette.gray600, Palette.gray100),
+  transparent(Palette.gray00, Colors.transparent),
+  delete(Palette.gray00, Palette.delete);
+
+  final Color color;
+  final Color backgroundColor;
+  const BoxButtonType(this.color, this.backgroundColor);
+}
