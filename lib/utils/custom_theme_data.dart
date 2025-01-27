91f4ffc2219f7ba207ee82ef6412fa3f7cd3cd6d
wonhj12 <wonhj12@naver.com>
2025-01-27 16:08:36 +0900

    design: 테마 색 수정 - AppBar scrolledUnderElevation 설정 - TextButton 색 변경 - 주석 추가

--- lib/utils/custom_theme_data.dart
+++ lib/utils/custom_theme_data.dart
@@ -4,21 +4,29 @@ import 'package:nutripic/utils/palette.dart';
 class CustomThemeData {
   static final ThemeData light = ThemeData(
     fontFamily: 'Pretendard',
+
+    // AppBar 테마
     appBarTheme: AppBarTheme(
-      titleTextStyle: Palette.heading.copyWith(color: Palette.green500),
+      titleTextStyle: Palette.body1.copyWith(color: Palette.gray800),
       centerTitle: true,
+      scrolledUnderElevation: 0,
+      foregroundColor: Palette.gray800,
       backgroundColor: Palette.gray00, // Appbar BG
     ),
+
+    // Scaffold 테마
     scaffoldBackgroundColor: Palette.gray00, // Scaffold BG
+
+    // 앱 전체적인 테마
     colorScheme: const ColorScheme.light(
-      primary: Palette.green700,
-      primaryContainer: Palette.green500,
-      secondary: Palette.green600,
+      // primary: Palette.green700,
+      // primaryContainer: Palette.green500,
+      // secondary: Palette.green600,
       error: Palette.delete,
     ),
     highlightColor: Colors.transparent, // Modal highlight color
     textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom(foregroundColor: Palette.green500),
+      style: TextButton.styleFrom(foregroundColor: Palette.gray400),
     ),
   );
 }
