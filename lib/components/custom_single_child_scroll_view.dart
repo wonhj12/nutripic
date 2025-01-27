19d46af748aa75c238288fc939a09a2e855bb54b
wonhj12 <wonhj12@naver.com>
2025-01-27 16:03:47 +0900

    feat: CustomScrollView 컴포넌트 구현

---
+++ lib/components/custom_single_child_scroll_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+/// `Spacer()` 사용이 가능하도록 화면 크기 계산하는 Scroll View
+class CustomSingleChildScrollView extends StatelessWidget {
+  final Widget child;
+  const CustomSingleChildScrollView({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) => SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(minHeight: constraints.maxHeight),
+          child: IntrinsicHeight(child: child),
+        ),
+      ),
+    );
+  }
+}
