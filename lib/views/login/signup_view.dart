1539cf91a83df2d90868e28944c5488f51f17766
wonhj12 <wonhj12@naver.com>
2025-01-28 14:37:28 +0900

    design: 회원가입 페이지 ui 디자인 변경

--- lib/views/login/signup_view.dart
+++ lib/views/login/signup_view.dart
@@ -1,62 +1,115 @@
 import 'package:flutter/material.dart';
 import 'package:nutripic/components/common/custom_app_bar.dart';
+import 'package:nutripic/components/common/custom_scaffold.dart';
+import 'package:nutripic/components/custom_single_child_scroll_view.dart';
+import 'package:nutripic/components/custom_text_field.dart';
+import 'package:nutripic/components/main_button.dart';
+import 'package:nutripic/utils/enums/main_button_type.dart';
+import 'package:nutripic/utils/enums/text_field_type.dart';
+import 'package:nutripic/utils/palette.dart';
 import 'package:nutripic/view_models/login/signup_view_model.dart';
 import 'package:provider/provider.dart';
 
-class SignupView extends StatefulWidget {
+class SignupView extends StatelessWidget {
   const SignupView({super.key});
 
   @override
-  State<SignupView> createState() => _SignupViewState();
-}
-
-class _SignupViewState extends State<SignupView> {
-  @override
   Widget build(BuildContext context) {
     SignupViewModel signupViewModel = context.watch<SignupViewModel>();
-    return Scaffold(
+
+    return CustomScaffold(
       appBar: const CustomAppBar(title: '회원가입'),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // 이름
-          const Text('이름'),
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (value) => signupViewModel.name = value,
-          ),
-          const SizedBox(height: 8),
+      resizeToAvoidBottomInset: true,
+      isLoading: signupViewModel.isLoading,
+      body: Form(
+        key: signupViewModel.formKey,
+        autovalidateMode: signupViewModel.validateMode,
+        child: CustomSingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 52),
 
-          // 이메일
-          const Text('이메일'),
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (value) => signupViewModel.email = value,
-          ),
-          const SizedBox(height: 8),
+              // 이름
+              CustomTextField(
+                label: '이름',
+                controller: signupViewModel.name,
+                validator: signupViewModel.validateName,
+                onChanged: signupViewModel.onTextFieldChanged(),
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                textFieldType: TextFieldType.text,
+              ),
+              const SizedBox(height: 24),
 
-          // 비밀번호
-          const Text('비밀번호'),
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (value) => signupViewModel.password = value,
-          ),
-          const SizedBox(height: 8),
+              // 이메일
+              CustomTextField(
+                label: '이메일',
+                controller: signupViewModel.email,
+                validator: signupViewModel.validateEmail,
+                onChanged: signupViewModel.onTextFieldChanged(),
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
+                textFieldType: TextFieldType.email,
+              ),
+              const SizedBox(height: 24),
 
-          // 비밀번호 확인
-          const Text('비밀번호 확인'),
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            onChanged: (value) => signupViewModel.passwordCheck = value,
-          ),
-          const SizedBox(height: 8),
+              // 비밀번호
+              CustomTextField(
+                label: '비밀번호',
+                controller: signupViewModel.password,
+                validator: signupViewModel.validatePassword,
+                onChanged: signupViewModel.onTextFieldChanged(),
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                textFieldType: TextFieldType.password,
+              ),
+              const SizedBox(height: 8),
 
-          // 회원가입
-          ElevatedButton(
-            onPressed: () => signupViewModel.signup(),
-            child: const Text('회원가입'),
+              // 비밀번호 도움 문구
+              Text(
+                '비밀번호는 최소 8자로 구성해야하며,\n특수문자, 숫자, 영어 중 2개 이상의 조건을 충족해야합니다.',
+                style: Palette.caption3.copyWith(color: Palette.gray400),
+              ),
+              const SizedBox(height: 24),
+
+              // 비밀번호 확인
+              CustomTextField(
+                label: '비밀번호 확인',
+                controller: signupViewModel.passwordCheck,
+                validator: signupViewModel.validatePasswordCheck,
+                onChanged: signupViewModel.onTextFieldChanged(),
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.done,
+                textFieldType: TextFieldType.password,
+              ),
+
+              // 오류 문구
+              if (signupViewModel.errorText != null)
+                Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Text(
+                    signupViewModel.errorText!,
+                    style: Palette.subbody1.copyWith(color: Palette.delete),
+                  ),
+                ),
+              const SizedBox(height: 24),
+              const Spacer(),
+
+              // 회원가입
+              MainButton(
+                label: '회원가입',
+                type: signupViewModel.isSignupBtnEnabled
+                    ? MainButtonType.enabled
+                    : MainButtonType.disabled,
+                onPressed: signupViewModel.signup,
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
