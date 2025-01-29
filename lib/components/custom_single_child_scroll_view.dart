2f7b73c766ba946e84ce688d2e6dcecceeada826
wonhj12 <wonhj12@naver.com>
2025-01-29 20:26:36 +0900

    mod: CustomSingleChildScrollView에 mainaxis, crossaxisalignment 추가

--- lib/components/custom_single_child_scroll_view.dart
+++ lib/components/custom_single_child_scroll_view.dart
@@ -3,7 +3,15 @@ import 'package:flutter/material.dart';
 /// `Spacer()` 사용이 가능하도록 화면 크기 계산하는 Scroll View
 class CustomSingleChildScrollView extends StatelessWidget {
   final List<Widget> children;
-  const CustomSingleChildScrollView({super.key, required this.children});
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+
+  const CustomSingleChildScrollView({
+    super.key,
+    required this.children,
+    this.mainAxisSize = MainAxisSize.min,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +20,12 @@ class CustomSingleChildScrollView extends StatelessWidget {
         child: ConstrainedBox(
           constraints: BoxConstraints(minHeight: constraints.maxHeight),
           child: IntrinsicHeight(
-              child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: children,
-          )),
+            child: Column(
+              mainAxisSize: mainAxisSize,
+              crossAxisAlignment: crossAxisAlignment,
+              children: children,
+            ),
+          ),
         ),
       ),
     );
