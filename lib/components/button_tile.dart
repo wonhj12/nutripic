7b3bab17a8bd73c3f98e68bd6a5e32956cd21a2b
wonhj12 <wonhj12@naver.com>
2025-01-30 13:54:36 +0900

    design ButtonTile 색 변경

--- lib/components/button_tile.dart
+++ lib/components/button_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nutripic/utils/palette.dart';
 
 /// 공지사항 등에서 사용되는 타일 버튼
 class ButtonTile extends StatelessWidget {
@@ -15,17 +16,21 @@ class ButtonTile extends StatelessWidget {
         height: 52,
         padding: const EdgeInsets.symmetric(horizontal: 16),
         decoration: BoxDecoration(
-          border: Border.all(width: 0.5, color: const Color(0xFFB5B5B5)),
+          border: Border.all(width: 0.5, color: Palette.gray200),
           borderRadius: BorderRadius.circular(10),
         ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             // 타일 제목
-            Text(title, style: const TextStyle(fontSize: 14)),
+            Text(title, style: Palette.body2.copyWith(color: Palette.gray900)),
 
             // 화살표 아이콘
-            const Icon(Icons.arrow_forward_ios, size: 12)
+            const Icon(
+              Icons.arrow_forward_ios,
+              size: 12,
+              color: Palette.gray900,
+            )
           ],
         ),
       ),
