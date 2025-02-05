f6d1a0eeef5de8df5793f56211422f8e8ab18fc6
onlyshine <remnant0709@naver.com>
2025-02-06 02:17:59 +0900

    feat: 레시피 완료 후 재확인 질문 화면 생성

--- lib/views/recipe/recipe_finish_view.dart
+++ lib/views/recipe/recipe_finish_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:nutripic/components/common/custom_app_bar.dart';
 import 'package:nutripic/components/common/custom_scaffold.dart';
 import 'package:nutripic/components/refrigerator/refrigerator_container.dart';
@@ -78,10 +79,7 @@ class RecipeFinishView extends StatelessWidget {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.pop(
-                      context,
-                      recipeFinishViewModel.filterSelectedFoods.toList(),
-                    );
+                    _showConfirmationDialog(context, recipeFinishViewModel);
                   },
                   child: const Text('적용하기'),
                 ),
@@ -89,6 +87,57 @@ class RecipeFinishView extends StatelessWidget {
             ),
         ],
       ),
+    );
+  }
+
+  /// 작은 확인 창을 띄우는 함수
+  void _showConfirmationDialog(
+      BuildContext context, RecipeFinishViewModel viewModel) {
+    showDialog(
+      context: context,
+      barrierDismissible: true, // 배경 클릭 시 닫기 가능
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('삭제 확인'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('선택한 음식을 삭제하시겠습니까?'),
+              const SizedBox(height: 16),
+              ...viewModel.filterSelectedFoods.map((food) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(food.name),
+                    Text(
+                      food.expireDate.toString(),
+                      style: const TextStyle(color: Colors.grey, fontSize: 12),
+                    ),
+                  ],
+                );
+              }),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // 다이얼로그 닫기
+              },
+              child: const Text('취소'),
+            ),
+            TextButton(
+              onPressed: () {
+                // 선택한 음식을 삭제
+                Navigator.pop(context); // 다이얼로그 닫기
+
+                context.go('/recipe'); // 다이얼로그 닫기
+                // 화면 닫기
+              },
+              child: const Text('확인'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
