45576b25690a6616d6c7449e66503d9f08be4f7f
wonhj12 <wonhj12@naver.com>
2025-02-06 15:42:58 +0900

    design: BoxButtonType에 primary 속성 생성

--- lib/utils/enums/box_button_type.dart
+++ lib/utils/enums/box_button_type.dart
@@ -5,7 +5,8 @@ import 'package:nutripic/utils/palette.dart';
 enum BoxButtonType {
   normal(Palette.gray600, Palette.gray100),
   transparent(Palette.gray00, Colors.transparent),
-  delete(Palette.gray00, Palette.delete);
+  delete(Palette.gray00, Palette.delete),
+  primary(Palette.green600, Palette.gray100);
 
   final Color color;
   final Color backgroundColor;
