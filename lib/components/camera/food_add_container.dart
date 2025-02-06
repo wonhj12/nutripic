b091110770a72789f7fed1268ae6bebbf0ceccc7
wonhj12 <wonhj12@naver.com>
2025-02-06 15:43:59 +0900

    mod: FoodAddContainer을 재활용해서 냉장, 냉동, 실온 보관 모두 표시할 수 있도록 수정

--- lib/components/camera/food_add_container.dart
+++ lib/components/camera/food_add_container.dart
@@ -1,52 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:nutripic/components/camera/recognized_food_tile.dart';
+import 'package:nutripic/objects/food.dart';
 import 'package:nutripic/utils/palette.dart';
 
 class FoodAddContainer extends StatelessWidget {
-  final Set<String> recognizedFoods;
-  const FoodAddContainer({super.key, required this.recognizedFoods});
+  final String title;
+  final List<Food> recognizedFoods;
+  final bool isSelectState;
+  const FoodAddContainer({
+    super.key,
+    required this.title,
+    required this.recognizedFoods,
+    required this.isSelectState,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        const SizedBox(height: 36),
+    return recognizedFoods.isNotEmpty
+        ? Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // 타이틀
+              Text(title, style: Palette.title1Medium),
+              const SizedBox(height: 16),
 
-        // 타이틀
-        Row(
-          children: [
-            const Text('냉장보관', style: Palette.title1Medium),
-            const Spacer(),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.add_rounded))
-          ],
-        ),
-        const SizedBox(height: 16),
+              // 인식한 식재료 컨테이너
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Palette.gray00,
+                  border: Border.all(color: Palette.gray200),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ListView.separated(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: recognizedFoods.length,
+                  separatorBuilder: (context, index) => const Divider(
+                    color: Palette.gray100,
+                    height: 12,
+                  ),
+                  itemBuilder: (BuildContext context, int index) =>
+                      RecognizedFoodTile(
+                    food: recognizedFoods.elementAt(index),
+                    isSelectState: isSelectState,
+                    onTapEdit: () {},
+                  ),
+                ),
+              ),
 
-        // 인식한 식재료 컨테이너
-        Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: Palette.gray00,
-            border: Border.all(color: Palette.gray200),
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: ListView.separated(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: recognizedFoods.length,
-            separatorBuilder: (context, index) => const SizedBox(height: 8),
-            itemBuilder: (BuildContext context, int index) =>
-                RecognizedFoodTile(
-              name: recognizedFoods.elementAt(index),
-              onTapEdit: () {},
-              onTapDelete: () {},
-            ),
-          ),
-        ),
-      ],
-    );
+              const SizedBox(height: 32),
+            ],
+          )
+        : Container();
   }
 }
