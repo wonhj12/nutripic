543fe9b705880b8d783eb1e8df2d1e90bf331b49
wonhj12 <wonhj12@naver.com>
2025-03-27 09:49:25 +0900

    design: 레시피 재료 컴포넌트 디자인 수정

--- lib/components/recipe/recipe_ingredients.dart
+++ lib/components/recipe/recipe_ingredients.dart
@@ -11,27 +11,25 @@ class RecipeIngredient extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 10),
-      width: MediaQuery.of(context).size.width * (343 / 375),
+      width: double.infinity,
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.black26, width: 0.7),
-        color: Colors.white, // 투명한 흰색 배경
-        borderRadius: BorderRadius.circular(16), // 모서리 둥글게
+        border: Border.all(color: Palette.gray300, width: 0.8),
+        borderRadius: BorderRadius.circular(10),
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // 제목
           const Text(
-            "재료",
-            style: TextStyle(
-              fontSize: 18, // 제목 글꼴 크기 수정
-              fontWeight: FontWeight.w600, // 강조된 제목
-            ),
+            '재료',
+            style: Palette.body1,
           ),
-          const Divider(
-            thickness: 1.0, // 구분선 두께 조정
-            color: Colors.black26,
-          ),
+          const SizedBox(height: 4),
+
+          Divider(thickness: 0.5, color: Palette.gray300),
           const SizedBox(height: 8),
+
+          // 재료
           Table(
             columnWidths: const {
               0: FlexColumnWidth(1), // 첫 번째 열 너비
@@ -73,8 +71,7 @@ class RecipeIngredient extends StatelessWidget {
       children: [
         Text(
           ingredient.ingredientName, // 재료 이름
-          style: Palette.subtitle2Medium
-              .copyWith(color: Palette.gray500, fontWeight: FontWeight.w500),
+          style: Palette.subtitle2Medium.copyWith(color: Palette.gray500),
         ),
         Text(
           ingredient.amount, // 재료 수량
